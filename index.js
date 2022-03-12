ef5ebde6e3dfacab502c29e3211b7b9ff15c0a6f
Reinel Torres <rey880603@gmail.com>
2022-03-11 20:12:07 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+    let jumpQty = 0
+    let i = 0
+    while (i<c.length-1) {
+        if (c[i+1]===0) {
+            if (c[i+2]===0) {
+                i = i + 2
+            } else {
+                i++
+            }
+        } else {
+            i = i + 2
+        }
+
+        jumpQty++
+    }
+
+    return jumpQty
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
